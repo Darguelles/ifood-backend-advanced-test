82086775669b717bbcd35ff0e388a39a1bcc26be
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2018-02-19 00:41:49 -0300

    Aplication is ready and configured for start development process

---
+++ src/main/java/com/ifood/IfoodMusicApplication.java
@@ -0,0 +1,12 @@
+package com.ifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodMusicApplication.class, args);
+	}
+}
