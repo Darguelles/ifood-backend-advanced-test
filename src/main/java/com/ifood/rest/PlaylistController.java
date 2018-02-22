019fabcd9f614bfa570f0bc407966f8ee3568689
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2018-02-22 19:28:17 -0300

    Helper methods are refactorized

--- src/main/java/com/ifood/rest/PlaylistController.java
+++ src/main/java/com/ifood/rest/PlaylistController.java
@@ -1,20 +1,8 @@
 package com.ifood.rest;
 
-import com.ifood.client.SpotifyClient;
-import com.ifood.model.SpotifyAuthCredentials;
-import feign.Feign;
-import feign.form.FormEncoder;
-import feign.gson.GsonDecoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlaylistController {
-
-    @GetMapping("/holi")
-    public String holi(){
-        System.out.println("HOLIIIII");
-        return "HOLIIII";
-    }
 
 }
--- src/main/java/com/ifood/util/Util.java
+++ src/main/java/com/ifood/util/Util.java
@@ -5,7 +5,7 @@ import java.util.Base64;
 public class Util {
 
     public static String getBase64EncodedString(String value) {
-        return Base64.getEncoder().encode("810d7196f9dd4b19b7dcf3c07d3dbc84:bb683aaf8f234e2a9774e9b60bf2b6fa".getBytes()).toString();
+        return Base64.getEncoder().encodeToString(value.getBytes());
     }
 
 }
