01204d127598eb1e27585d67e193374d3bf98018
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2018-02-23 00:17:48 -0300

    Playlist response correctly mapped and represented in objects

---
+++ src/main/java/com/ifood/model/Track.java
@@ -0,0 +1,43 @@
+package com.ifood.model;
+
+import java.util.List;
+
+public class Track {
+
+    private Album album;
+    private List<Artist> artists;
+    private String name;
+
+    public Track() {
+    }
+
+    public Track(Album album, List<Artist> artists, String name) {
+        this.album = album;
+        this.artists = artists;
+        this.name = name;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(List<Artist> artists) {
+        this.artists = artists;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
